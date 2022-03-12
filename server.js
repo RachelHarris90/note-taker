231e9a3ab7e59fb93255549fdf59ac2314fdb6a4
Rachel Harris <rachel.harris@msn.com>
2022-03-12 21:08:50 +1100

    Add uuid to saved notes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const notesData = require('./db/db.json')
 const fs = require('fs');
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuid4 } = require('uuid');
 
 const app = express();
 const PORT = 3001;
