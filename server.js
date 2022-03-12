e9a40c06b24b0a54b4c2bcfffd55137661841b94
Rachel Harris <rachel.harris@msn.com>
2022-03-12 19:03:11 +1100

    Set up the Express app to handle data parsing

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require('express');
+const fs = require('fs');
 const path = require('path');
-const db = require('./db/db.json')
+const notesData = require('./db/db.json')
 
 const app = express();
 const PORT = 3001;
 
-
-// this will be used to store and retrieve notes using the fs module
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/INDEX.html'))
+  res.sendFile(path.join(__dirname, 'public/index.html'))
 );
 
 app.get('/notes', (req, res) =>
@@ -20,15 +21,14 @@ app.get('/notes', (req, res) =>
 
 // Get notes json
 app.get('/api/notes', (req, res) => 
-    // fs
+    res.json(notesData)
 );
 
-app.post('/api/notes', (req, res) => 
-  //
-);
+// TODO - post request
+// app.post('/api/notes', (req, res) => {
+//   notesData = JSON.parse(data);
+// })
 
 app.listen(PORT, () => 
     console.log(`App running on http://localhost:${PORT}`)
 );
-
-// 
